315409f3cf784ae7c1add2ac7cac6e3d9faf097a
sarmandasi <44706590+sarmandasi@users.noreply.github.com>
2019-07-06 06:19:21 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('mn onlinem');
+  	}
+});
+
+client.on('msg', msg => {
+    if (msg.content === 'key') {
+    	msg.reply('Slaw mn MrBot, Sarmand(MR PANDA) druste krdum :)');
+  	}
+});
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
