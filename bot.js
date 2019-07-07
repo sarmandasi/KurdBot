866631519612244055195839b64dc79345b4e0a8
sarmandasi <44706590+sarmandasi@users.noreply.github.com>
2019-07-07 13:48:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,12 +11,43 @@ client.on('message', message => {
   	}
 });
 
+
 client.on('msg', msg => {
     if (msg.content === 'key') {
     	msg.reply('Slaw mn MrBot, Sarmand(MR PANDA) druste krdum :)');
   	}
 });
+bot.on("message", message => {
+  let args = message.content.substring(PREFIX.length).split(" ");
 
+  switch (args[0]) {
+    case "kick":
+      if (!args[1]) message.channel.send("You need to Specify a person");
+      const user = message.mentions.users.first();
+
+      if (user) {
+        const member = member.guild.member(usre);
+
+        if (memebr) {
+          member
+            .kick("You where kick for tralling")
+            .then(() => {
+              message.reply("successfully kicked ${user.tag}");
+            })
+            .catch(err => {
+              message.replay("i was unable to kick the member");
+              console.log(err);
+            });
+        } else {
+          message.reply("That user isn't in the this guild");
+        }
+      } else {
+        message.reply("That user isn't in the this guild");
+      }
+
+      break;
+  }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
